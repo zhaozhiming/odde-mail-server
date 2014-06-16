0a68d3e5692e6015e9ea4ace960002f4fd040132
zhaozhiming <kingzzm1982@sina.com>
2014-06-16 21:50:53 +0800

    add recipient Jmockit controller test

---
+++ src/test/java/com/odde/mail/controller/RecipientControllerJMockitTest.java
@@ -0,0 +1,59 @@
+package com.odde.mail.controller;
+
+import com.odde.mail.model.Recipient;
+import com.odde.mail.model.Result;
+import com.odde.mail.service.RecipientService;
+import mockit.Injectable;
+import mockit.NonStrictExpectations;
+import mockit.Tested;
+import mockit.integration.junit4.JMockit;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JMockit.class)
+public class RecipientControllerJMockitTest {
+
+    @Tested
+    private RecipientController recipientController;
+
+    @Injectable
+    private RecipientService recipientService;
+
+    @Test
+    public void should_return_status_success_when_add_recipient_success() throws Exception {
+        verifyAddRecipientByResult("成功");
+    }
+
+    @Test
+    public void should_return_status_failed_when_add_recipient_failed() throws Exception {
+        verifyAddRecipientByResult("失败");
+    }
+
+    private void verifyAddRecipientByResult(final String serviceResult) throws Exception {
+        new NonStrictExpectations() {{
+            recipientService.add("Tom", "test@test.com");
+            result = new Result(serviceResult);
+        }};
+
+        String result = recipientController.add("Tom", "test@test.com");
+        assertThat(result, is("{\"status\":\"" + serviceResult + "\"}"));
+    }
+
+    @Test
+    public void should_return_recipients_when_list_success() throws Exception {
+        final Recipient tom = new Recipient("Tom", "test@test.com");
+        new NonStrictExpectations() {{
+            recipientService.list();
+            result = asList(tom);
+        }};
+
+        String result = recipientController.list();
+        assertThat(result, containsString("\"username\":\"Tom\",\"email\":\"test@test.com\""));
+    }
+
+}
