1c75fc2278cc27ee9a2c4b7d4464d5c04043dbf4
zhaozhiming <kingzzm1982@sina.com>
2014-06-17 12:23:58 +0800

    fix failed test

--- src/test/java/com/odde/mail/service/MailServiceTest.java
+++ src/test/java/com/odde/mail/service/MailServiceTest.java
@@ -10,8 +10,10 @@ import org.junit.runner.RunWith;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Session;
 import javax.mail.Transport;
-
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.Properties;
 
 import static org.hamcrest.core.Is.is;
@@ -29,34 +31,46 @@ public class MailServiceTest {
     @Mocked
     private Properties properties;
 
+    @Mocked
+    private Session session;
+
+    @Mocked
+    private MimeMessage mimeMessage;
+
+    @Mocked
+    private InternetAddress address;
+
     @Test
     public void should_return_success_when_send_mail_success() throws Exception {
-        new Expectations() {{
-            Transport.send((Message) any);
-        }};
+        mockSend(null);
 
         Result result = mailService.send("test@163.com", "test", "test");
+
         assertThat(result.getStatus(), is("成功"));
     }
 
     @Test
     public void should_return_success_when_send_multiple_mails_success() throws Exception {
-        new Expectations() {{
-            Transport.send((Message) any);
-        }};
+        mockSend(null);
 
         Result result = mailService.send("test@163.com,test@sina.com", "test", "test");
+
         assertThat(result.getStatus(), is("成功"));
     }
 
     @Test
     public void should_return_failed_when_send_mail_failed() throws Exception {
-        new Expectations() {{
-            Transport.send((Message) any);
-            result = new MessagingException();
-        }};
+        mockSend(new MessagingException());
 
         Result result = mailService.send("wrong", "test", "test");
+
         assertThat(result.getStatus(), is("失败"));
+    }
+
+    private void mockSend(final MessagingException exception) throws MessagingException {
+        new Expectations() {{
+            Transport.send((Message) any);
+            result = exception;
+        }};
     }
 }
